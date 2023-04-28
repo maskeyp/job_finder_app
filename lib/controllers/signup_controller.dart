9cf1b1b5a819f1ccc04eb9614bbcedcb7aa40983
maskeyp <59556770+maskeyp@users.noreply.github.com>
2023-04-28 20:41:57 +0545

    Delete signup_controller.dart

--- lib/controllers/signup_controller.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter/widgets.dart';
-// import 'package:get/get.dart';
-
-// class SignUpController extends GetxController {
-//   static SignUpController get instance => Get.find();
-//   final email = TextEditingController();
-//   final password = TextEditingController();
-// }
